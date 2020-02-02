0008ab1579b43b42402edbf0aec30e9195157faa
SHOTORAK\Saeid <saostad2@gmail.com>
2020-02-01 22:39:40 -0500

    download for 1 file is working added progress bar

--- dist/app.js
+++ dist/app.js
@@ -16,28 +16,100 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const fast_node_logger_1 = require("fast-node-logger");
 dotenv_1.default.config();
 const puppeteer_1 = __importDefault(require("puppeteer"));
+const http_1 = __importDefault(require("http"));
+const url_1 = __importDefault(require("url"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const cli_progress_1 = __importDefault(require("cli-progress"));
+/**Configs */
+const fetchUrl = "https://satina.website/download-friends/";
+const elementToFindInPage = "a";
+const propToSearchInElements = "href";
+const keyWord = "1080";
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield fast_node_logger_1.createLogger();
+        const logger = yield fast_node_logger_1.createLogger({
+            prettyPrint: {
+                colorize: true,
+                translateTime: "SYS:standard",
+            },
+        });
         /** put your code below here */
+        fast_node_logger_1.writeLog("Lunching Chromium...", { stdout: true });
         const browser = yield puppeteer_1.default.launch();
         const page = yield browser.newPage();
-        yield page.goto("https://satina.website/download-friends/", {
+        fast_node_logger_1.writeLog(`opening page ${fetchUrl}`, { stdout: true });
+        yield page.goto(fetchUrl, {
             waitUntil: "networkidle2",
         });
-        const hyperlinks = yield page.$$("a");
+        /**get all html a elements */
+        const hyperlinks = yield page.$$(elementToFindInPage);
+        /**filter s elements with content of their link contain '1080'
+         * for just getting full hd links
+         * otherwise return false
+         */
+        fast_node_logger_1.writeLog(`searching for keyword '${keyWord}' in href of a HTML tags`, {
+            stdout: true,
+        });
         const data = hyperlinks.map((el) => __awaiter(this, void 0, void 0, function* () {
-            const prop = yield el.getProperty("href");
+            const prop = yield el.getProperty(propToSearchInElements);
             const hrefLink = (yield prop.jsonValue());
-            if (hrefLink.includes("1080")) {
+            if (hrefLink.includes(keyWord)) {
                 return hrefLink;
             }
             return false;
         }));
         const links = yield Promise.all(data);
-        const correctLinks = links.filter(el => el !== false);
-        console.log(`File: app.ts,`, `Line: 32 => `, correctLinks);
         yield browser.close();
+        /** filter raw kinks to get actual links */
+        const rawLinks = links.filter(el => el !== false);
+        const linkToDownload = rawLinks[1];
+        const { protocol, hostname, pathname } = url_1.default.parse(linkToDownload);
+        const progress = new cli_progress_1.default.SingleBar({ format: "progress [{bar}] {percentage}% | {value}/{total}" }, cli_progress_1.default.Presets.shades_classic);
+        fast_node_logger_1.writeLog(`downloading file ${linkToDownload}`, { stdout: true });
+        http_1.default.get(linkToDownload, res => {
+            var _a;
+            if (res.statusCode === 302) {
+                const newLocation = res.headers.location;
+                const redirectedUrl = `${protocol}//${hostname}${newLocation}`;
+                const filename = (_a = pathname) === null || _a === void 0 ? void 0 : _a.slice(pathname.lastIndexOf("/") + 1);
+                const pathInFS = path_1.default.join(process.cwd(), "downloads", filename);
+                http_1.default.get(redirectedUrl, res => {
+                    if (res.statusCode === 200) {
+                        /** file size in bytes */
+                        const fileSize = res.headers["content-length"];
+                        /** convert byte to mega byte */
+                        const byteToMB = (input) => {
+                            let number;
+                            if (typeof input === "number") {
+                                number = input;
+                            }
+                            else {
+                                number = parseInt(input);
+                            }
+                            const result = (number / 1024 / 1024).toFixed(2);
+                            return Number(result);
+                        };
+                        const fileInFS = fs_1.default.createWriteStream(pathInFS);
+                        let totalReceivedData = 0;
+                        progress.start(byteToMB(fileSize), 0);
+                        res.on("error", function (err) {
+                            progress.stop();
+                            console.error(err);
+                        });
+                        res.on("data", function dataChunkHandler(chunk) {
+                            totalReceivedData += chunk.length;
+                            progress.update(byteToMB(totalReceivedData));
+                        });
+                        res.on("end", function end() {
+                            progress.stop();
+                            console.log(`${filename} successfully downloaded`);
+                        });
+                        res.pipe(fileInFS);
+                    }
+                });
+            }
+        });
     });
 }
 exports.main = main;
--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,127 @@
 import dotenv from "dotenv";
-import { createLogger } from "fast-node-logger";
-
+import { createLogger, writeLog } from "fast-node-logger";
 dotenv.config();
-
 import puppeteer from "puppeteer";
+import http from "http";
+import url from "url";
+import fs from "fs";
+import path from "path";
+import cliProgress from "cli-progress";
+
+/**Configs */
+const fetchUrl = "https://satina.website/download-friends/";
+const elementToFindInPage = "a";
+const propToSearchInElements = "href";
+const keyWord = "1080";
+const defaultDownloadDir = path.join(process.cwd(), "downloads");
+const downloadDir = undefined;
 
 export async function main() {
-  await createLogger();
+  const logger = await createLogger({
+    prettyPrint: {
+      colorize: true,
+      translateTime: "SYS:standard",
+    },
+  });
 
   /** put your code below here */
-
+  writeLog("Lunching Chromium...", { stdout: true });
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://satina.website/download-friends/", {
+
+  writeLog(`opening page ${fetchUrl}`, { stdout: true });
+  await page.goto(fetchUrl, {
     waitUntil: "networkidle2",
   });
 
-  const hyperlinks = await page.$$("a");
+  /**get all html a elements */
+  const hyperlinks = await page.$$(elementToFindInPage);
+
+  /**filter s elements with content of their link contain '1080'
+   * for just getting full hd links
+   * otherwise return false
+   */
+  writeLog(`searching for keyword '${keyWord}' in href of a HTML tags`, {
+    stdout: true,
+  });
   const data = hyperlinks.map(async el => {
-    const prop = await el.getProperty("href");
+    const prop = await el.getProperty(propToSearchInElements);
     const hrefLink = (await prop.jsonValue()) as string;
-    if (hrefLink.includes("1080")) {
+    if (hrefLink.includes(keyWord)) {
       return hrefLink;
     }
     return false;
   });
 
   const links = await Promise.all(data);
-  const correctLinks = links.filter(el => el !== false);
-  console.log(`File: app.ts,`, `Line: 32 => `, correctLinks);
-
   await browser.close();
+
+  /** filter raw kinks to get actual links */
+  const rawLinks = links.filter(el => el !== false) as string[];
+
+  const linkToDownload = rawLinks[1];
+
+  const { protocol, hostname, pathname } = url.parse(linkToDownload);
+
+  const progress = new cliProgress.SingleBar(
+    { format: "progress [{bar}] {percentage}% | {value}/{total}" },
+    cliProgress.Presets.shades_classic,
+  );
+
+  writeLog(`downloading file ${linkToDownload}`, { stdout: true });
+  http.get(linkToDownload, res => {
+    if (res.statusCode === 302) {
+      const newLocation = res.headers.location;
+
+      const redirectedUrl = `${protocol}//${hostname}${newLocation}`;
+
+      const filename = pathname?.slice(pathname.lastIndexOf("/") + 1) as string;
+
+      const pathInFS = path.join(downloadDir ?? defaultDownloadDir, filename);
+      http.get(redirectedUrl, res => {
+        if (res.statusCode === 200) {
+          /** file size in bytes */
+          const fileSize = res.headers["content-length"] as string;
+
+          /** convert byte to mega byte */
+          const byteToMB = (input: number | string): number => {
+            let number: number;
+            if (typeof input === "number") {
+              number = input;
+            } else {
+              number = parseInt(input);
+            }
+            const result = (number / 1024 / 1024).toFixed(2);
+
+            return Number(result);
+          };
+
+          const fileInFS = fs.createWriteStream(pathInFS);
+          let totalReceivedData = 0;
+          progress.start(byteToMB(fileSize), 0);
+
+          res.on("error", function(err) {
+            progress.stop();
+            console.error(err);
+          });
+
+          res.on("data", function dataChunkHandler(chunk) {
+            totalReceivedData += chunk.length;
+            progress.update(byteToMB(totalReceivedData));
+          });
+
+          res.on("end", function end() {
+            progress.stop();
+            writeLog(`${filename} successfully downloaded`, { stdout: true });
+          });
+
+          res.pipe(fileInFS);
+        }
+      });
+    }
+  });
 }
 
 main().catch(err => {
-  console.log(`File: app.ts,`, `Line: 35 => `, err);
+  writeLog(err);
 });
